d2337ed96249686ec8c17e56812f981153adc8dc
00andrusha <merabishvili.a123@gmail.com>
2026-01-31 15:00:54 +0400

    project start

---
+++ Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int health;
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        StartCoroutine(FlashRed());
+     
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    System.Collections.IEnumerator FlashRed()
+    {
+        Renderer rend = GetComponentInChildren<Renderer>();
+        if (rend != null)
+        {
+            Color originalColor = rend.material.color;
+            rend.material.color = Color.red;
+            yield return new WaitForSeconds(0.1f);
+            rend.material.color = originalColor;
+        }
+    }
+}
+
